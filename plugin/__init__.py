8f8551a284693929699a0bea1b7fcf4ea0964487
Orlandoxx <95180049+Orlandoxx@users.noreply.github.com>
2025-01-14 11:02:31 +0200

    Add files via upload

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,41 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-import gettext
-import os
+from gettext import gettext, dgettext, bindtextdomain
 
-
-PluginLanguageDomain = 'Foreca'
-PluginLanguagePath = 'Extensions/Foreca/locale'
-
-
-isDreambox = False
-if os.path.exists("/usr/bin/apt-get"):
-	isDreambox = True
+PluginLanguageDomain = "Foreca"
+PluginLanguagePath = "Extensions/Foreca/locale"
 
 
 def localeInit():
-	if isDreambox:
-		lang = language.getLanguage()[:2]
-		os.environ["LANGUAGE"] = lang
-	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+    bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
-if isDreambox:
-	def _(txt):
-		return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
-else:
-	def _(txt):
-		translated = gettext.dgettext(PluginLanguageDomain, txt)
-		if translated:
-			return translated
-		else:
-			print(("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)))
-			return gettext.gettext(txt)
+def _(txt):
+    t = dgettext(PluginLanguageDomain, txt)
+    if t == txt:
+        #print "[%s] fallback to default translation for %s" %(PluginLanguageDomain, txt)
+        t = gettext(txt)
+    return t
+
 
 localeInit()
 language.addCallback(localeInit)
-# URL del file remoto che contiene la nuova base URL
-file_url = "https://raw.githubusercontent.com/Belfagor2005/e2openplugin-Foreca/refs/heads/master/plugin/foreca_v.txt"
